aeac2eb7c6c0034245c0bd5866ec755b498444f0
John Nicholas <john.nicholas@dinoroar.io>
2023-06-09 19:42:31 +0100

    simple event registration

--- store/eventRouter.spec.js
+++ store/eventRouter.spec.js
@@ -1,6 +1,23 @@
-describe('readmodels want to specify what events they recievee', ()=>{
-    it("registers for event type 'BasketCreated' and gets the event",()=>{
 
-        expect('BasketCreated').toEqual('BasketCreated')
-    })
-})
+
+describe("readmodels want to specify what events they recievee", () => {
+  it("registers for event type 'BasketCreated' and gets the event", () => {
+    let router = {};
+    let result;
+    const toCall = (e) => {
+      result = e;
+    };
+    const withRegistration = registerForEvent(router, "BasketCreated", toCall);
+    handleEvent(withRegistration, {eventType: 'BasketCreated'})
+
+    expect("BasketCreated").toEqual("BasketCreated");
+  });
+});
+
+function registerForEvent(router, eventType, toCall) {
+  return { ...router, [eventType]: toCall };
+}
+
+function handleEvent(router, event){
+    router[event.eventType](event)
+}
