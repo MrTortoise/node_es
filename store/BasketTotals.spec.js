dd40e07942907909893e75460818ac9a2f631f8b
John Nicholas <john.nicholas@dinoroar.io>
2023-06-09 17:21:53 +0100

    test actually passing

--- store/BasketTotals.spec.js
+++ store/BasketTotals.spec.js
@@ -1,8 +1,7 @@
 describe("basket totals tells people how much is in users baskets", () => {
   it("have a value of 0 for a newly created basket", () => {
-    const dave = { total: 1 };
-  
-
+    const dave = { total: 0 };
+    const basket = {dave}
     expect(basket.dave.total).toBe(0);
   });
 });
