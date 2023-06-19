0652c0ddf60fbe7acb8a77674b1ed81e2ffa2a16
John Nicholas <john.nicholas@dinoroar.io>
2023-06-19 20:00:13 +0100

    comment

--- identity/RequestCreatedWorker.js
+++ identity/RequestCreatedWorker.js
@@ -1,4 +1,9 @@
 export class RequestCreatedWorker {
+    // this is a worker ... workers turn events (or readmodels) into commands
+    // it takes an event and executes a command against a different stream
+    // process managers are examples of more complex workers
+    // personally I prefer to use process managers when there is a a,b,c,done sequence
+    // over saga and choreography
     constructor(eventStore, commandRouter, api) {
         this.eventStore = eventStore;
         this.commandRouter = commandRouter;
